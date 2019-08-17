e15b94c5067b5436404471cc4b92363847a9369e
Abraham Arias <arias.abram@gmail.com>
2019-08-16 23:00:23 -0700

    adding Javascript LinkedList prePend method

--- myLinkedList.js
+++ myLinkedList.js
@@ -1,5 +1,12 @@
 // add a method prepend() to the linked list that adds a node to the beginning of the list
 
+class Node {
+	constructor(value){
+		this.value = value;
+		this.next = null;
+	}
+}
+
 class LinkedList {
 	constructor(value) {
 		this.head = {
@@ -14,17 +21,25 @@ class LinkedList {
 			value: value,
 			next: null
 		};
-		console.log(newNode);
 		this.tail.next = newNode;
 		this.tail = newNode;
 		this.length++;
 		return this;
 	}
+
 	prepend(value) {
-		//TODO
+		const newNode = {
+			value: value,
+			next: null
+		};
+		newNode.next = this.head;
+		this.head = newNode;
+		this.length++;
+		return this;
 	}
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
-myLinkedList.append(16);myLinkedList.prepend(1)
+myLinkedList.append(16);
+myLinkedList.prepend(1);
